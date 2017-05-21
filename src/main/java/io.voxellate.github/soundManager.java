e2ebef88cdbbdf7487f3865280e7add50dfd2952
Voxellate <LukeDugdale@icloud.com>
2017-05-21 18:57:11 +1000

    Finished commenting

--- src/main/java/io.voxellate.github/soundManager.java
+++ src/main/java/io.voxellate.github/soundManager.java
@@ -12,20 +12,20 @@ class soundManager {
 
     void playSound(String sprite, String state, boolean loop) {
         try {
-            InputStream audioSrc = getClass().getResourceAsStream("sounds/"+ sprite + "_" + state +".wav");
-            InputStream bufferedIn = new BufferedInputStream(audioSrc);
-            AudioInputStream audioStream = AudioSystem.getAudioInputStream(bufferedIn);
-            Clip clip = AudioSystem.getClip();
-            clip.open(audioStream);
-            if (loop) {clip.loop(-1);}
-            clip.start();
-            if (Objects.equals(state, "bgMusic")){bgMusic = clip;}
-        } catch (LineUnavailableException | IOException | UnsupportedAudioFileException e) {
-            e.printStackTrace();
+            InputStream audioSrc = getClass().getResourceAsStream("sounds/"+ sprite + "_" + state +".wav");     //Read the audio file requested and store them in an input stream
+            InputStream bufferedIn = new BufferedInputStream(audioSrc);     //Buffer the input stream
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(bufferedIn); //Turn the buffered input stream into an audio stream
+            Clip clip = AudioSystem.getClip();  //Create a new clip object
+            clip.open(audioStream);     //Open the audio stream
+            if (loop) {clip.loop(-1);}  //If the gameController requests the clip to be looped, set the clip to loop
+            clip.start();   //Play the clip
+            if (Objects.equals(state, "bgMusic")){bgMusic = clip;}  //If the music to be played is the background music, store the clip in a local attribute
+        } catch (LineUnavailableException | IOException | UnsupportedAudioFileException e) {    //If any of these errors are encountered...
+            e.printStackTrace();    //Print the stack trace to the console for debugging
         }
     }
 
     void stopSound(){
         bgMusic.stop();
-    }
+    }   //Stop the background music
 }
--- src/main/java/io.voxellate.github/wordManager.java
+++ src/main/java/io.voxellate.github/wordManager.java
@@ -6,50 +6,49 @@ import java.util.*;
 class wordManager {
 
     String selectedWord;
-    private static StringBuilder censoredWord = new StringBuilder();
+    private static StringBuilder censoredWord = new StringBuilder();    //Creates a new StringBuilder to contain the censored word
     private static JLabel wordLabel;
 
-    wordManager (JLabel label) {
+    wordManager (JLabel label) {    //Constructor for class
         selectedWord = null;
-
-        Scanner sc = new Scanner(getClass().getResourceAsStream("files/wordList.txt"));
-        List<String> lines = new ArrayList<>();
-        while (sc.hasNextLine()) {
-            lines.add(sc.nextLine());
+        Scanner sc = new Scanner(getClass().getResourceAsStream("files/wordList.txt"));     //Receives the contents of the file as an input stream and parses it using a Scanner
+        List<String> lines = new ArrayList<>();     //Creates a List of strings
+        while (sc.hasNextLine()) {      //Loop through each item in the file scanner
+            lines.add(sc.nextLine());   //Add the line to the List as a new element
         }
-        String[] wordList = lines.toArray(new String[0]);
-        selectedWord = wordList[new Random().nextInt(wordList.length)];
-        System.out.println(selectedWord);
+        String[] wordList = lines.toArray(new String[0]);   //Convert the list to an array
+        selectedWord = wordList[new Random().nextInt(wordList.length)]; //Pick a random array element to be the word to guess
+        System.out.println(selectedWord);   //Print that word to the console for debugging
 
-        censoredWord = new StringBuilder();
-        wordLabel = label;
-        for (int i = 0; i < selectedWord.length(); i++) {
-            String s = selectedWord.substring(i, i + 1);
-            if (s.equals(" ") || s.equals(",")) {
-                censoredWord.append(s);
-            } else {
-                censoredWord.append("_");
+        censoredWord = new StringBuilder(); //Create a new string builder, which allows for easy manipulation of strings
+        wordLabel = label;  //Make label an attribute so it may be accessed by other methods
+        for (int i = 0; i < selectedWord.length(); i++) {   //Loop through each character in the selected word
+            String s = selectedWord.substring(i, i + 1);    //Equal s to a character in the selected word
+            if (s.equals(" ") || s.equals(",")) {   //If that character is a space or a comma...
+                censoredWord.append(s);     //append that character to censored word
+            } else {    //Otherwise...
+                censoredWord.append("_");   //add an underscore to the censored word
             }
         }
-        wordLabel.setText(censoredWord.toString());
+        wordLabel.setText(censoredWord.toString());     //Set the word label to equal the censored word
     }
 
-    boolean wordCompare(char a) {
-        boolean b = false;
-        String l = String.valueOf(a);
-        for (int i = 0; i < selectedWord.length(); i++) {
-            String s = selectedWord.substring(i, i + 1);
-            if (s.equals(l)) {
-                censoredWord.replace(i, i + 1, s);
+    boolean wordCompare(char a) {   //Compares the guessed letter to the letters in the word to guess
+        boolean b = false;  //set boolean b to false, which means that the letter was not found in the word
+        String l = String.valueOf(a);   //Change a from a char to a string
+        for (int i = 0; i < selectedWord.length(); i++) {   //For each character in the selected word
+            String s = selectedWord.substring(i, i + 1);    //Equal s to a character in the selected word
+            if (s.equals(l)) {      //If the character equals the selected letter...
+                censoredWord.replace(i, i + 1, s);  //Replaced the corresponding underscores with the letter in the censored word
                 b = true;
             }
         }
-        wordLabel.setText(censoredWord.toString());
+        wordLabel.setText(censoredWord.toString());     //Set the word label to equal the new censored word
         return b;
     }
 
     boolean wordCheck(){
-        return Objects.equals(censoredWord.toString(), selectedWord);
+        return Objects.equals(censoredWord.toString(), selectedWord);   //Return true if the censored word equals the selected word
     }
 }
 
